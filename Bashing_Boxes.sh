962668731e0ad8ae4734a210993d6ff65b10c2c5
anabsyaz <anabsyaz@gmail.com>
2025-10-14 10:49:59 -0500

    Started making a function to print the list

--- Bashing_Boxes.sh
+++ Bashing_Boxes.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-
+#This is the array for the random objects
 RandomItems=("Dresser" "Jar" "Chocolate bar" "Sofa" "Apple tree" "Shawl" "Butterfly" "Underwear" "Coat rack" "Hair dryer") 
+
+print_list() {
+	echo "Current object"
+	
+}
