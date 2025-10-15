3c1cfc4e2279919b1e56c7a8adbe859fee286c84
anabsyaz <anabsyaz@gmail.com>
2025-10-14 21:05:45 -0500

    Realized I made a mistake when I was calling on the array from the script

--- Bashing_Boxes.sh
+++ Bashing_Boxes.sh
@@ -6,16 +6,16 @@ RandomItems=("Dresser" "Jar" "Chocolate bar" "Sofa" "Apple tree" "Shawl" "Butter
 #shows the full list of objects
 print_list() {
   echo "Here is the list:"
-  for i in "${!boxes[@]}"; do
-    echo "$i: ${boxes[$i]}"
+  for i in "${!RandomItems[@]}"; do
+    echo "$i: ${RandomItems[$i]}"
   done
 }
 
 #shows one object by its number
 print_item() {
   read -p "Enter the number of the item: " number
-  if [[ $number -ge 0 && $number -lt ${#boxes[@]} ]]; then
-    echo "Item $number is ${boxes[$number]}"
+  if [[ $number -ge 0 && $number -lt ${#RandomItems[@]} ]]; then
+    echo "Item $number is ${RandomItems[$number]}"
   else
     echo "That number doesn’t exist."
   fi
@@ -24,24 +24,46 @@ print_item() {
 # add a new item
 add_item() {
   read -p "Enter something to add: " thing
-  boxes+=("$thing")
+  RandomItems+=("$thing")
   echo "$thing added to the list."
 }
 
 # remove the last item
 remove_last() {
-  unset 'boxes[-1]'
+  unset 'RandomItems[-1]'
   echo "Last item removed."
 }
 
 # remove an item by its number
 remove_item() {
   read -p "Enter the number of the item to remove: " num
-  if [[ $num -ge 0 && $num -lt ${#boxes[@]} ]]; then
-    echo "${boxes[$num]} was removed."
-    unset 'boxes[num]'
-    boxes=("${boxes[@]}")
+  if [[ $num -ge 0 && $num -lt ${#RandomItems[@]} ]]; then
+    echo "${RandomItems[$num]} was removed."
+    unset 'RandomItems[num]'
+    RandomItems=("${RandomItems[@]}")
   else
     echo "That number doesn’t exist."
   fi
 }
+
+while true; do
+  echo
+  echo "BASHING BOXES MENU"
+  echo "1. Print list"
+  echo "2. Print one item"
+  echo "3. Add new item"
+  echo "4. Remove last item"
+  echo "5. Remove item by number"
+  echo "6. Exit"
+  read -p "Pick an option (1-6): " choice
+
+  case $choice in
+    1) print_list ;;
+    2) print_item ;;
+    3) add_item ;;
+    4) remove_last ;;
+    5) remove_item ;;
+    6) echo "Bye."; exit ;;
+    *) echo "Not a valid choice." ;;
+  esac
+done
